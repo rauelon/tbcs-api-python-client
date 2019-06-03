bb37e0abe7175a31f81daa5b2fcda57ff871c339
janj <jan.junge@imbus.de>
2019-06-03 17:08:22 +0200

    Removed explicit versioning for dependencies.

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='tbcs_api_client',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
 
-    install_requires=['requests>=2.22.0'],
+    install_requires=['requests'],
 
     description="Basic api client for automated testing with TestBench CS",
     license="PSF",
