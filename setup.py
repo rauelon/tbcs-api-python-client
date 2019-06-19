dbc9276546126184feea15d94f03488c68a760f3
janj <jan.junge@imbus.de>
2019-06-19 10:24:44 +0200

    Added dependency to setup.py.

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='tbcs_api_client',
-    version='0.10',
+    version='0.11',
     packages=find_packages(exclude=['tests']),
 
-    install_requires=['requests'],
+    install_requires=['requests', 'urllib3'],
 
     description="Basic api client for automated testing with TestBench CS",
     license="PSF",
