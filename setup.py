3dbe4129e0002883e0632ebef38fd2d9fbe960b8
janj <jan.junge@imbus.de>
2019-07-08 16:34:57 +0200

    Added version numbers to dependencies.

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(
     version='0.13',
     packages=find_packages(exclude=['tests']),
 
-    install_requires=['requests', 'urllib3'],
+    python_requires=">=3.6",
+    install_requires=['requests>=2.22.0'],
 
     description="Basic api client for automated testing with TestBench CS",
     license="PSF",
