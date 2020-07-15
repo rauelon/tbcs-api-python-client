7a34ebef8837ff2534e92ea52258a3dc37989525
Jonas Butz <jonasb@imbus.de>
2020-07-15 10:53:40 +0200

    Adopted setup configuration

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tbcs_api_client',
-    version='0.17',
+    version='0.18',
     packages=find_packages(exclude=['tests']),
 
     python_requires=">=3.6",
