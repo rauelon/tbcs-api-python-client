2184699475afb4c82c2171947363a4c7edc7ddb6
janj <jan.junge@imbus.de>
2021-03-09 16:56:53 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tbcs_api_client',
-    version='0.20',
+    version='0.21',
     packages=find_packages(exclude=['tests']),
 
     python_requires=">=3.8",
